f9b7375453406e989541ed5c835d032454ed35f7
duo <hypersonolabs@gmail.com>
2022-05-12 17:26:49 +0900

    add static index.html file and render it on page

--- src/app.js
+++ src/app.js
@@ -1,31 +1,45 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
+const path = require("path");
 
 const init = async () => {
   const server = Hapi.server({
     port: 8000,
     host: "localhost",
+    routes: { files: { relativeTo: path.join(__dirname, "static") } },
+  });
+
+  //plugin
+  await server.register({
+    plugin: require("@hapi/inert"),
   });
 
   //routes
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "hello hapi world";
+  server.route([
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, h) => {
+        return "hello hapi world";
+      },
     },
-  });
-
-  //POST routes
-  server.route({
-    method: "POST",
-    path: "/signup",
-    handler: (request, h) => {
-      const payload = request.payload;
-      return payload;
+    {
+      method: "POST",
+      path: "/signup",
+      handler: (request, h) => {
+        const payload = request.payload;
+        return payload;
+      },
     },
-  });
+    {
+      method: "GET",
+      path: "/static",
+      handler: (request, h) => {
+        return h.file("index.html");
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`server is listening at ${server.info.uri}`);
